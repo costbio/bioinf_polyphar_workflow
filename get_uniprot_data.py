10c03b2918754824bc07c718ec9a03cce0511be8
Şuara Şahin <shnsuara@gmail.com>
2023-07-24 13:02:44 +0300

    Missing imports are added

--- get_uniprot_data.py
+++ get_uniprot_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import re
 
 def checkgenes(gene_path, in_csv):
     # Open the file containing the words to search for
